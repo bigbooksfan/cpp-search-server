3609f753b677f936ea6fdfb1872b2daad342c205
bigbooksfan <90531040+bigbooksfan@users.noreply.github.com>
2022-12-24 15:23:03 +0300

    Add files via upload

--- search-server/paginator.h
+++ search-server/paginator.h
@@ -2,6 +2,39 @@
 
 #include <vector>
 
+template<typename Iterator>
+class IteratorRange {                   // kinda wrapper
+public:
+    IteratorRange(Iterator begin, Iterator end) : begin_{ begin }, end_{ end } {};
+    IteratorRange() {
+
+    }
+
+public:
+    Iterator begin() const {
+        return begin_;
+    }
+
+    Iterator end() const {
+        return end_;
+    }
+
+    Iterator size() const {
+        return end_ - begin_;
+    }
+
+private:
+    Iterator begin_;
+    Iterator end_;
+
+};
+
+template<typename Iterator>
+std::ostream& operator<< (std::ostream& os, const IteratorRange<Iterator>& out) {
+    for (Iterator It = out.begin(); It < out.end(); ++It) os << *It;
+    return os;
+}
+
 template <typename Iterator>
 class Paginator {
 public:
@@ -47,10 +80,4 @@ private:
 template <typename Container>
 auto Paginate(const Container& c, size_t page_size) {
     return Paginator(begin(c), end(c), page_size);
-}
-
-template<typename Iterator>
-std::ostream& operator<< (std::ostream& os, const IteratorRange<Iterator>& out) {
-    for (Iterator It = out.begin(); It < out.end(); ++It) os << *It;
-    return os;
 }
--- search-server/string_processing.h
+++ search-server/string_processing.h
@@ -2,10 +2,5 @@
 
 #include <string>
 #include <vector>
-#include <iostream>
-
-std::string ReadLine();
-int ReadLineWithNumber();		// нет, это часть обработки string со входа из консоли. 
-//Из этой функции вызывается Readline(). Разве не будет лучше, если они будут лежать рядом?
 
 std::vector<std::string> SplitIntoWords(const std::string& text);
