ff7c051fa00871788609ee9681a304c536035fce
bigbooksfan <90531040+bigbooksfan@users.noreply.github.com>
2023-01-08 21:17:42 +0300

    review2

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -3,14 +3,27 @@
 #include <vector>
 #include <iostream>
 
-void RemoveDuplicates(SearchServer& search_server)
-{
+void RemoveDuplicates(SearchServer& search_server) {
+    std::set<std::set<std::string>> word_sets;
+    std::vector<int> ids_to_remove;
 
-	std::vector<int> ids_to_remove = search_server.CheckDuplicatesInside();
+    std::set<std::string> doc_words;
 
-	for (int i : ids_to_remove) {
-		search_server.RemoveDocument(i);
-		std::cout << "Found duplicate document id " << i << std::endl;
-	}
+    for (const int doc_id : search_server) {
+        for (const std::pair<std::string, double>& i : search_server.GetWordFrequencies(doc_id)) {
+            doc_words.insert(i.first);
+        }
+        if (word_sets.count(doc_words) == 0) {
+            word_sets.insert(doc_words);
+        }
+        else {
+            ids_to_remove.push_back(doc_id);
+        }
+        doc_words.clear();
+    }
 
+    for (int i : ids_to_remove) {
+        search_server.RemoveDocument(i);
+        std::cout << "Found duplicate document id " << i << std::endl;
+    }
 }
--- search-server/search_server.cpp
+++ search-server/search_server.cpp
@@ -2,6 +2,9 @@
 
 #include <algorithm>
 
+// static member definition
+std::map<int, std::map<std::string, double>> SearchServer::words_freqs_overall_;
+
 std::vector<int>::const_iterator SearchServer::begin()
 {
     return ids_.begin();
@@ -12,12 +15,9 @@ std::vector<int>::const_iterator SearchServer::end()
     return ids_.end();
 }
 
-const std::map<std::string, double> SearchServer::GetWordFrequencies(int document_id) const
+const std::map<std::string, double>& SearchServer::GetWordFrequencies(int document_id) const
 {
-    std::map<std::string, double> ret;
-    for (const auto word : word_to_document_freqs_) {
-        if (word.second.count(document_id)) ret.emplace(word.first, word.second.at(document_id));
-    }
+    std::map<std::string, double>& ret = words_freqs_overall_[document_id];
     return ret;
 }
 
@@ -25,19 +25,22 @@ void SearchServer::RemoveDocument(int document_id)
 {
 
     for (auto word : word_to_document_freqs_) {
-        if (word.second.count(document_id)) word.second.erase(document_id);
-    }
-    if (documents_.count(document_id)) documents_.erase(document_id);
-    
-    for (std::vector<int>::iterator It = ids_.begin(); It != ids_.end(); ) {
-        if (*It == document_id) {
-            It = ids_.erase(It);
-            //continue;
+        if (word.second.count(document_id)) {
+            word.second.erase(document_id);
         }
-        else ++It;
     }
-
-    if (words_of_docs_.count(document_id)) words_of_docs_.erase(document_id);
+    if (documents_.count(document_id)) {
+        documents_.erase(document_id);
+    }
+    
+    for (std::vector<int>::iterator it = ids_.begin(); it != ids_.end(); ) {
+        if (*it == document_id) {
+            it = ids_.erase(it);
+        }
+        else {
+            ++it;
+        }
+    }
 
 }
 
@@ -47,52 +50,17 @@ void SearchServer::SetStopWords(const std::string& text) {
     }
 }
 
-const std::vector<int> SearchServer::CheckDuplicatesInside()
-{
-    std::vector<int> ret;
-
-    std::map<int, std::set<std::string>>::const_iterator pre_end = words_of_docs_.end();
-    if (pre_end != words_of_docs_.begin()) --pre_end;
-    else throw std::out_of_range("Something wrong with (words_of_docs_) size!");
-
-    for (std::map<int, std::set<std::string>>::const_iterator It_slow = words_of_docs_.begin();
-        It_slow != pre_end; ++It_slow) {
-
-        for (std::map<int, std::set<std::string>>::const_iterator It_fast = It_slow;
-            It_fast != pre_end; ) {
-
-            ++It_fast;
-            
-            if ((*It_slow).second.size() != (*It_fast).second.size()) continue;
-            // different number of unique words. Not a twins
-
-            bool flag = true;
-
-            for (std::set<std::string>::const_iterator It_word = (*It_slow).second.begin();
-                It_word != (*It_slow).second.end(); ++It_word) {
-
-                flag = true;
-                if ((*It_fast).second.count(*It_word) == 0) {
-                    flag = false;
-                    break;      // slow and fast - not a twins
-                }
-            }
-
-            if (flag) {
-                int twin_id = std::max((*It_slow).first, (*It_fast).first);
-                ret.push_back(twin_id);
-            }
-        }
-    }
-
-    return ret;
-}
-
 void SearchServer::AddDocument(int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings) {
 
-    if (document_id < 0) throw std::invalid_argument("Negative ID");
-    if (documents_.count(document_id)) throw std::invalid_argument("ID already exist");
-    if (!IsValidWord(document)) throw std::invalid_argument("Special symbol in AddDocument");
+    if (document_id < 0) {
+        throw std::invalid_argument("Negative ID");
+    }
+    if (documents_.count(document_id)) {
+        throw std::invalid_argument("ID already exist");
+    }
+    if (!IsValidWord(document)) {
+        throw std::invalid_argument("Special symbol in AddDocument");
+    }
 
     ids_.push_back(document_id);
 
@@ -101,10 +69,14 @@ void SearchServer::AddDocument(int document_id, const std::string& document, Doc
     for (const std::string& word : words) {
         word_to_document_freqs_[word][document_id] += inv_word_count;
     }
+
     documents_.emplace(document_id, DocumentData{ ComputeAverageRating(ratings), status });
 
-    std::set<std::string> wordset(words.begin(), words.end());
-    words_of_docs_.emplace(document_id, wordset);
+    std::map<std::string, double> wordset;
+    for (const std::string& word : words) {
+        wordset.emplace(word, word_to_document_freqs_[word][document_id]);
+    }
+    words_freqs_overall_.emplace(document_id, wordset);
 }
 
 std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_query, DocumentStatus stat) const {
@@ -142,15 +114,10 @@ std::tuple<std::vector<std::string>, DocumentStatus> SearchServer::MatchDocument
     return std::make_tuple(matched_words, documents_.at(document_id).status);
 }
 
-//int SearchServer::GetDocumentId(int index) const {
-//    if (index < 0 || index > documents_.size()) {
-//        throw std::out_of_range("Wrong document id in GetDocumentId()");
-//    }
-//    return ids_[index];
-//}
-
 SearchServer::SearchServer(const std::string& stop_words) {          // explicit
-    if (!IsValidWord(stop_words)) throw std::invalid_argument("Special symbol in constructor (Yandex method)");
+    if (!IsValidWord(stop_words)) {
+        throw std::invalid_argument("Special symbol in constructor (Yandex method)");
+    }
     SetStopWords(stop_words);
 }
 
@@ -178,7 +145,6 @@ int SearchServer::ComputeAverageRating(const std::vector<int>& ratings) {  // st
 
 SearchServer::QueryWord SearchServer::ParseQueryWord(std::string text) const {
     bool is_minus = false;
-    // Word shouldn't be empty
     if (text[0] == '-') {
         is_minus = true;
         text = text.substr(1);
@@ -192,10 +158,15 @@ SearchServer::Query SearchServer::ParseQuery(const std::string& text) const {
     for (const std::string& word : SplitIntoWords(text)) {
         const QueryWord query_word = ParseQueryWord(word);
 
-        if (query_word.is_minus && query_word.data[0] == '-') throw std::invalid_argument("Double minus in ParseQuery()");
-        if (!IsValidWord(query_word.data)) throw std::invalid_argument("Special symbol in ParseQuery()");
-        if (query_word.is_minus && query_word.data.empty()) throw std::invalid_argument("No word after minus in ParseQuery()");
-
+        if (query_word.is_minus && query_word.data[0] == '-') {
+            throw std::invalid_argument("Double minus in ParseQuery()");
+        }
+        if (!IsValidWord(query_word.data)) {
+            throw std::invalid_argument("Special symbol in ParseQuery()");
+        }
+        if (query_word.is_minus && query_word.data.empty()) {
+            throw std::invalid_argument("No word after minus in ParseQuery()");
+        }
 
         if (!query_word.is_stop) {
             if (query_word.is_minus) {
@@ -218,30 +189,4 @@ bool SearchServer::IsValidWord(const std::string& word) {          // static
     return std::none_of(word.begin(), word.end(), [](char c) {
         return c >= '\0' && c < ' ';
         });
-}
-
-[[nodiscard]] bool SearchServer::CheckDoubleMinus(const std::string& in) const {
-    bool prev_minus = false;
-    for (const char letter : in) {
-        if (letter == '-') {
-            if (prev_minus == true) {
-                return false;
-            }
-            else prev_minus = true;
-        }
-        else prev_minus = false;
-    }
-
-    return true;
-}
-
-[[nodiscard]] bool SearchServer::CheckNoMinusWord(const std::string& in) const {
-    const unsigned int j = static_cast<unsigned int>(in.size());
-    if (in[j - 1] == '-') return false;
-
-    for (unsigned int i = 0; i < j; ++i) {
-        if (in[i] == '-') if (in[i + 1] == ' ') return false;
-    }
-
-    return true;
 }
